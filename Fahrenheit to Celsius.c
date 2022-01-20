cd967125d3fefa125495b7dcb75a33a285fcf388
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-01-20 09:11:55 +0530

    #include<stdio.h> //#define int main(){     //int r;     float area,r;     //pi = (22/7);     scanf("%f",&r);     area=((r-32)*5)/9;     printf("%.2f",area);     return 0;
    }

---
+++ Fahrenheit to Celsius.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//#define 
+int main(){
+    //int r;
+    float area,r;
+    //pi = (22/7);
+    scanf("%f",&r);
+    area=((r-32)*5)/9;
+    printf("%.2f",area);
+    return 0;
+    
+    
+}
