c2019072947de18f83bbc8cd80597a9e1a9a80d9
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-02-26 23:43:16 +0530

    #include<stdio.h> int main(){     int n,m,i,j,arr[100][100],sum=0;     scanf("%d %d",&n,&m);     for(i=0;i<n;i++){         for(j=0;j<m;j++){             scanf("%d",&arr[i][j]);         }     }      for(i=0;i<n;i++){         for(j=0;j<m;j++){             sum=sum+arr[i][j];         }     }     printf("%d",sum);     return 0; }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,m,i,j,arr[100][100],sum=0;
+    scanf("%d
+%d",&n,&m);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            sum=sum+arr[i][j];
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
