1efa25737eb9895ee89b6954bf5aa5f857e2b305
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-28 18:30:18 +0530

    #include<stdio.h> int main(){     int n,m;     scanf("%d %d",&n,&m);     int a[n][m];     for(int i=0;i<n;i++){         for(int j=0;j<m;j++){             scanf("%d ",&a[i][j]);         }     }     int sum=0,sum1=0;     for(int i=0;i<n;i++){         for(int j=0;j<m;j++){             sum+=a[i][j];         }     }     for(int i=0;i<n;i++){         for(int j=0;j<m;j++){             if(i==0 || j==0 || i==n-1|| j==m-1)             sum1+=a[i][j];         }     }     printf("%d",sum-sum1);     return 0; }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d ",&a[i][j]);
+        }
+    }
+    int sum=0,sum1=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum+=a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0 || j==0 || i==n-1|| j==m-1)
+            sum1+=a[i][j];
+        }
+    }
+    printf("%d",sum-sum1);
+    return 0;
+}
