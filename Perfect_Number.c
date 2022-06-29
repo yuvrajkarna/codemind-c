eaf7aa69c51073e2497a369a2ccce83eed1fafb2
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-29 11:52:00 +0530

    #include<stdio.h> int main(){     int num,a=0,t=0,num1,i;     scanf("%d",&num);     num1=num;     for(i=1;i<num;i++){         t=num%i;         if(t==0){             a=i+a;             //a=i;         }     }     if(a==num){         printf("True");     }     else{         printf("False");     }     return 0; }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,23 +1,20 @@
 #include<stdio.h>
-#include<math.h>
-
-int fac(int p){
-    int i,sum=0;
-    for(i=1;i<p;i++){
-        if(p%i==0){
-            sum=sum+i;
+int main(){
+    int num,a=0,t=0,num1,i;
+    scanf("%d",&num);
+    num1=num;
+    for(i=1;i<num;i++){
+        t=num%i;
+        if(t==0){
+            a=i+a;
+            //a=i;
         }
     }
-    return sum;
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int d=fac(n);
-    if(n==d){
+    if(a==num){
         printf("True");
     }
-    else
-    printf("False");
+    else{
+        printf("False");
+    }
     return 0;
 }
