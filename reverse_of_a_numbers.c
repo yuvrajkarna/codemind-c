5eccc42d53e598e19ce01b36a1ab84ee07cebac5
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-29 12:54:56 +0530

    #include<stdio.h> int main(){     int num,t,sum=0;     scanf("%d",&num);     while(num>0){         t=num%10;         sum=(sum*10)+t;         num=num/10;     }     printf("%d",sum);     return 0; }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num,t,sum=0;
+    scanf("%d",&num);
+    while(num>0){
+        t=num%10;
+        sum=(sum*10)+t;
+        num=num/10;
+    }
+    printf("%d",sum);
+    return 0;
+}
+    
