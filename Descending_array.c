c4d189144727299479d998079c1a93d00ace33a6
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-30 23:15:36 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d",&a[i]);     }    int flag=0;     for(int i=0;i+1<n;i++){
            if(a[i]<a[i+1]){
                flag=1;
                printf("no");
                break;
            }
    
        }
        if(flag==0)
        printf("yes");
        
        
        return 0;
    }

---
+++ Descending_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+   int flag=0;
+    for(int i=0;i+1<n;i++){
+       
+        if(a[i]<a[i+1]){
+            flag=1;
+            printf("no");
+            break;
+        }
+
+    }
+    if(flag==0)
+    printf("yes");
+    
+    
+    return 0;
+}
