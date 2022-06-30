c5efe54956cffad1975f2057a3e1750434c719b3
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-30 22:55:06 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d",&a[i]);     }     int n1,n2,f=0;     scanf("%d %d",&n1,&n2);     if(n1>n2){         for(int i=0;i<n;i++){             if(a[i]>n1 ||a[i]<n2){                 f+=a[i];             }
            }
        }
        else{
            for(int i=0;i<n;i++){
                if(a[i]<n1 ||a[i]>n2){
                    f+=a[i];
                } 
            }
        }
        printf("%d",f);
        
        return 0;
    }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int n1,n2,f=0;
+    scanf("%d %d",&n1,&n2);
+    if(n1>n2){
+        for(int i=0;i<n;i++){
+            if(a[i]>n1 ||a[i]<n2){
+                f+=a[i];
+            }
+       
+        }
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(a[i]<n1 ||a[i]>n2){
+                f+=a[i];
+            } 
+        }
+    }
+    printf("%d",f);
+    
+    return 0;
+}
