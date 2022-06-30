5eae48862f8a6c07be5dfd573df3f12adec21a5d
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-30 11:51:43 +0530

    #include<stdio.h> int isPrime(int n){     if(n<=1) return 0;     for(int i=2;i*i<=n;i++){         if(n%i==0) return 0;     }     return 1; } int main(){     int n,t,c=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     scanf("%d ",&a[i]);     int key,min=9999999,max=0;     scanf("%d",&key);
        for(int i=0;i<n;i++)
        {
            if(isPrime(a[i])){
                c++;
            }
        }
        printf("%d",c);
        return 0;
    }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int isPrime(int n){
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main(){
+    int n,t,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    scanf("%d ",&a[i]);
+    int key,min=9999999,max=0;
+    scanf("%d",&key);
+   
+    for(int i=0;i<n;i++)
+    {
+        if(isPrime(a[i])){
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
