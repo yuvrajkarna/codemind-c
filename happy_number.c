e818b778b325b29e482b50da161056afd0c3f369
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-30 10:53:52 +0530

    #include<stdio.h> int add(int n){     int r,s=0;     while(n){         r=n%10;         s=s+r*r;         n/=10;     }     if(s>=10){         return add(s);     }else{         return s;     } } int main(){     int n;     scanf("%d",&n);     if(add(n)==1 || add(n)==7)     printf("True");     else     printf("False");     return 0; }

---
+++ happy_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int add(int n){
+    int r,s=0;
+    while(n){
+        r=n%10;
+        s=s+r*r;
+        n/=10;
+    }
+    if(s>=10){
+        return add(s);
+    }else{
+        return s;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(add(n)==1 || add(n)==7)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
