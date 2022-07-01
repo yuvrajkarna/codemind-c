1533f072a35bc0206b5001e5c48d0df054704d3e
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-07-01 13:55:45 +0530

    #include<stdio.h> int main() {     int i,n,a[100],s=0,k=0,j,b[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             b[k]=a[i];             k++;         }     }     for(i=0;i<k;i++)     {         for(j=0;j<k;j++)         {             if(b[i]==b[j]&&i!=j)             {                 b[j]=-1;             }         }     }     for(i=0;i<k;i++)     {         if(b[i]!=-1)         {             c++;         }     }     printf("%d",c); }

---
+++ unique_even_count.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[100],s=0,k=0,j,b[100],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            b[k]=a[i];
+            k++;
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(b[i]==b[j]&&i!=j)
+            {
+                b[j]=-1;
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        if(b[i]!=-1)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
