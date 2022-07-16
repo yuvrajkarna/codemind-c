1438989ca35792d24cad49493d609cb8506bcffd
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-07-16 11:31:07 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d ",&a[i]);     }int k,f=0;     scanf("%d ",&k);     for(int i=0;i<n;i++){         if(a[i]==k){             f=1;             printf("%d ",i);             break;         }     }     if(f==0)     printf("-1");     return 0; }

---
+++ Search_in_Rotated_Sorted_Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d ",&a[i]);
+    }int k,f=0;
+    scanf("%d ",&k);
+    for(int i=0;i<n;i++){
+        if(a[i]==k){
+            f=1;
+            printf("%d ",i);
+            break;
+        }
+    }
+    if(f==0) 
+    printf("-1");
+    return 0;
+}
