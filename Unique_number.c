a1696e02bfee3f1abcec00359f6d2b4e897d9c45
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-10-14 17:49:35 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int c=0,b,i,j,temp,flag=0;     temp=a;     while(temp>0)     {         b=temp%10;         c+=1;         temp=temp/10;     }     int arr[c];     temp=a;     for(i=0;i<c;i++)     {         b=temp%10;         arr[i]=b;         temp=temp/10;     }     for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             if(i!=j)             {                 if(arr[i]==arr[j])                 {                     flag=1;                     break;                 }             }         }         if(flag==1)         {             break;         }     }     if(flag==1)     {         printf("Not Unique Number");
        }
        else
        {
            printf("Unique Number");
        }
        return 0;
    }

---
+++ Unique_number.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int c=0,b,i,j,temp,flag=0;
+    temp=a;
+    while(temp>0)
+    {
+        b=temp%10;
+        c+=1;
+        temp=temp/10;
+    }
+    int arr[c];
+    temp=a;
+    for(i=0;i<c;i++)
+    {
+        b=temp%10;
+        arr[i]=b;
+        temp=temp/10;
+    }
+    for(i=0;i<c;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i]==arr[j])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==1)
+        {
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("Not Unique Number");
+        
+    }
+    else
+    {
+        printf("Unique Number");
+    }
+    return 0;
+}
